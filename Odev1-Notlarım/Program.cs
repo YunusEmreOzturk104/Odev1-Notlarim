82866eb05c0f14b19cb108a2c71f8eff609f8966
YunusEmreOzturk104 <ozturkyunusemre511@gmail.com>
2023-01-10 22:06:31 +0300

    Add project files.

---
+++ Odev1-Notlarım/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev1_Notlarım
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            
+        }
+    
+       public int SingleNumber(int[] nums)
+       {
+            int sonuc = 0;
+            foreach (var num in nums)
+            {
+                sonuc = num ^ sonuc;
+
+            }
+            return sonuc;
+       }
+        public bool checkPosibility(int[] nums)
+        {
+            int changedElement = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < nums[i - 1])
+                {
+                    changedElement++;
+                    if (changedElement > 1)
+                    {
+                        return false;
+                    }
+                    if (i > 1 && nums[i] <= nums[i - 2])
+                    {
+                        nums[i] = nums[i - 1];
+                    }
+                }
+            }
+            return changedElement <= 1;
+        }
+        }
+        
+    }
+}
